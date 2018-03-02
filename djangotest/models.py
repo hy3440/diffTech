8255de2ca5f96e372e17057ee1af4e6097d0cf8b
Lynn <lintiannil@gmail.com>
2018-03-02 17:31:24 +0800

    add new model id_postidtype

--- djangotest/models.py
+++ djangotest/models.py
@@ -6,3 +6,6 @@ class tags(models.Model):
     Category = models.CharField(max_length = 50)
     TagWiki = models.TextField()
     
+class id_postidtypes(models.Model):
+    postid = models.CharField(max_length = 2)
+    postidtype = models.CharField(max_length = 2)
--- djangotest/views.py
+++ djangotest/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
-from .models import tags
+from .models import tags, id_postidtypes
 
 # Create your views here.
 
 def home(request):
     Tags = tags.objects.all()
-    
+    id_postidtypes = id_postidtypes.objects.all()
+
     #return render(request, 'home.html',{'tags':Tags})
-    return render(request, 'home.html',{'tag':Tags})
+    return render(request, 'home.html',{'id_postidtypes':id_postidtypes})
 
 def tag_detail(request, id):
     try:
