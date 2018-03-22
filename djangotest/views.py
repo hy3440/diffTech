bf3d91bddf73a5e7867692be5b8335233444c050
Lynn <lintiannil@gmail.com>
2018-03-22 22:11:49 +0800

    successfully reference source title

--- djangotest/views.py
+++ djangotest/views.py
@@ -76,10 +76,10 @@ def tagcompare(request,tag,simi):
         """
         #make a dictionary containing relation of quality, id, example
         features = {}
-        IDS = []
+        IDS_noset = [] #1162378,1795117,1338597,259517,1338597
         for eachone in Relation:
             postid = eachone['example_id']
-            IDS.append(postid)
+            IDS_noset.append(int(postid))
 
             #info = SITE.fetch('posts/{ids}/revisions', ids = IDS)
             """
@@ -109,18 +109,38 @@ def tagcompare(request,tag,simi):
                 else:
                     features[qualityCate][quality] = [[eachone['example'],eachone['example_id']]]
 
-        #IDS_sorted = sorted(IDS, reverse = True)
-        info1 = SITE.fetch('posts/{ids}/revisions', ids = IDS) #the returning list of dictionary will be in descending order of the id
-        info2 = SITE.fetch('answers/{ids}/questions', ids = IDS)
+        # #IDS_sorted = sorted(IDS, reverse = True)
+        # #take care of repetitive answers id
+        # ids_count = {}
+        # for x in IDS_noset:
+        #     ids_count[x] = IDS_noset.count(x)
+
+        # IDSset = set(IDS_noset)
+        # info1 = SITE.fetch('/posts/{ids}/revisions', ids = IDSset ,order = 'desc', sort = 'creation') #the returning list of dictionary will be in descending order of the id
+        
         id_title = {} #id of post and title of that post, put into a dictionary
-        for item in info1['items']:
-            if 'title' in item.keys():
-                id_title[str(item['post_id'])] = item['title']
-                IDS.remove(str(item['post_id']))
-        IDS_sorted = sorted(IDS, reverse = True)                    
-        for item in info2['items']: #appears in descending order
-            if 'title' in item.keys():
-                id_title[IDS_sorted.pop(0)] = item['title']
+        # questionid = []
+        # for item in info1['items']:
+        #     if 'title' in item.keys():
+        #         id_title[item['post_id']] = item['title']
+        #         #ids_count[item['post_id']] -= 1
+        #         #if ids_count == 0:
+        #         questionid.append(item['post_id'])
+        #         IDSset.remove(item['post_id'])
+
+        # IDS_sorted = sorted(IDSset, reverse = True)
+        # info2 = SITE.fetch('/answers/{ids}/questions', ids = IDS_sorted , order = 'desc', sort = 'creation')    
+
+
+
+        # count_title = 0 
+        # len_ids_sorted = len(IDS_sorted)               
+        # for item in info2['items']: #appears in ascending order
+        #     if 'title' in item.keys():
+        #         count_title+=1
+        #         id_title[IDS_sorted.pop(0)] = item['title']
+        # if tested:
+        #     print('im')
 
         #if IDS_sorted != []:
         #    raise Http404("Number of titles not equal to number if IDS")
@@ -128,31 +148,53 @@ def tagcompare(request,tag,simi):
 
         # featureswithtitle = features.copy()
         # i = 0
-        for post_id, title in id_title.items():
-            #i+=1
-            found = 0 #find location for title to be attached
+        # for post_id, title in id_title.items():
+        #     #i+=1
+        #     found = 0 #find location for title to be attached
 
-            #locate its position in features dictionary
-            for qualityCate, qualityDict in features.items():
-                #if qualityCate not in featureswithtitle.keys():
-                    #featureswithtitle[qualityCate] = qualityDict
-                for quality, details in qualityDict.items():
-                    for item in details:
-                        if post_id == item[1]:
+        #     #locate its position in features dictionary
+        #     for qualityCate, qualityDict in features.items():
+        #         #if qualityCate not in featureswithtitle.keys():
+        #             #featureswithtitle[qualityCate] = qualityDict
+        #         for quality, details in qualityDict.items():
+        #             for item in details:
+        #                 if str(post_id) == item[1]:
+        #                     item.append(title)
+        #                     #featureswithtitle[qualityCate][quality][index]=['title','title']
+
+        #                     found = 1
+        #                     break
+        #             if found:
+        #                 break
+        #         if found:
+        #             break
+        #     if not found:
+        #         #if not tested: 
+        #         raise Http404("Dictionary pair not found for post_id "+ str(post_id))
+
+        # if not tested:
+        #     raise Http404('lalal')
+        IDS = sorted(IDS_noset)
+        info = SITE.fetch('/posts/{ids}',ids = IDS, filter = '!9Z(-wsMqT')
+        for item in info['items']:
+            id_title[item['post_id']] = item['title']
+
+
+
+        for qualityCate, qualityDict in features.items():
+            for quality, details in qualityDict.items():
+                for item in details:
+                    found = 0
+                    for post_id, title in id_title.items():
+                        if str(post_id) == item[1]:
                             item.append(title)
-                            #featureswithtitle[qualityCate][quality][index]=['title','title']
-
                             found = 1
                             break
-                    if found:
-                        break
-                if found:
-                    break
-            if not found:
-                raise Http404("Dictionary pair not found for post_id "+ str(post_id))
+                    if not found:
+                        #if tested: 
+                        raise Http404("Dictionary pair not found for "+ str(item[0]+' '+str(item[1])))
 
-        # if iamok:
-        #     print('lala')
+
 
 
 
@@ -195,7 +237,7 @@ def selecttag(request):
 
     if request.method == "POST":
 
-        Tag = request.POST.get('tag')
+        Tag = request.POST.get('tag').lower()
 
         SITE = StackAPI('stackoverflow')
         ori_tag = [Tag]
