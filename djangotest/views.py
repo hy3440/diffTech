c8b11cae2dc4eeef2fe3e3a2a583f89812f87770
Yi  Huang <yi.huang@anu.edu.au>
2018-09-03 20:17:21 +1000

    test new url

--- djangotest/views.py
+++ djangotest/views.py
@@ -46,20 +46,17 @@ def tagpair(request,Tag):
 
     return render(request, 'tagpair.html',{'tagsWikiDicts':tagsWikiDict,'ori_tagwikis':ori_tagwiki})
 
-# def tagcompare(request, twotags=None):
 
-    # if twotags is None:
-    #
-    #     tag = request.POST.get('tag').lower().strip()
-    #     simi = request.POST.get('simi').lower().strip()
-    #
-    # else:
-    #     twotags = twotags.split("&")
-    #     tag = twotags[0]
-    #     simi = twotags[1]
-def tagcompare(request,tag,simi):
+# def tagcompare(request,tag,simi):
 
-    tpair = sorted([tag, simi])
+def tagcompare(request, twotags):
+
+
+    # twotags = twotags.split("&")
+    # tag = twotags[0]
+    # simi = twotags[1]
+
+    tpair = sorted(twotags.split("&"))
     Tag = tpair[0]
     SimiTag = tpair[1]
     SITE = StackAPI('stackoverflow')
