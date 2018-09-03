5dff3c660949dede5490402c378cef68731917d9
Yi  Huang <yi.huang@anu.edu.au>
2018-09-03 22:15:03 +1000

    test

--- djangotest/views.py
+++ djangotest/views.py
@@ -247,7 +247,7 @@ def tagcomparepost(request):
 
         Relation = relation.objects.filter(tag = Tag, simitag = SimiTag).values('quality','example_id','example')
         if Relation:
-            return HttpResponseRedirect("/pairs/"+Tag+"&"+SimiTag+"/")
+            return HttpResponseRedirect("/"+Tag+"/"+SimiTag+"/")
             # return HttpResponseRedirect("/"+Tag+"/"+SimiTag+"/")
         else:
             error = {}
