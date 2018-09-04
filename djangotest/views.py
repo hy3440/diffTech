e7a2e7ed623a33775db55cd649f5ef9f3e03742e
Yi  Huang <yi.huang@anu.edu.au>
2018-09-04 15:15:25 +1000

    test another line chart

--- djangotest/views.py
+++ djangotest/views.py
@@ -9,36 +9,29 @@ import random
 import datetime
 import time
 
-def demo_linewithfocuschart(request):
+def demo_linechart(request):
     """
-    linewithfocuschart page
+    lineChart page
     """
-    nb_element = 100
     start_time = int(time.mktime(datetime.datetime(2012, 6, 1).timetuple()) * 1000)
-
+    nb_element = 100
     xdata = range(nb_element)
     xdata = map(lambda x: start_time + x * 1000000000, xdata)
     ydata = [i + random.randint(1, 10) for i in range(nb_element)]
     ydata2 = map(lambda x: x * 2, ydata)
-    ydata3 = map(lambda x: x * 3, ydata)
-    ydata4 = map(lambda x: x * 4, ydata)
 
     tooltip_date = "%d %b %Y %H:%M:%S %p"
-    extra_serie = {"tooltip": {"y_start": "There are ", "y_end": " calls"},
+    extra_serie = {"tooltip": {"y_start": "", "y_end": " cal"},
                    "date_format": tooltip_date}
-    chartdata = {
-        'x': xdata,
-        'name1': 'series 1', 'y1': ydata, 'extra1': extra_serie,
-        'name2': 'series 2', 'y2': ydata2, 'extra2': extra_serie,
-        'name3': 'series 3', 'y3': ydata3, 'extra3': extra_serie,
-        'name4': 'series 4', 'y4': ydata4, 'extra4': extra_serie
-    }
-    charttype = "lineWithFocusChart"
+    chartdata = {'x': xdata,
+                 'name1': 'series 1', 'y1': ydata, 'extra1': extra_serie,
+                 'name2': 'series 2', 'y2': ydata2, 'extra2': extra_serie}
+    charttype = "lineChart"
     data = {
         'charttype': charttype,
         'chartdata': chartdata
     }
-    return render_to_response('linewithfocuschart.html', data)
+    return render_to_response('linechart.html', data)
 # Create your views here.
 def robots(request):
 
