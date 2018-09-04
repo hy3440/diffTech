52118f782705496f6d1133e1a53cf6d8d025588c
Yi  Huang <yi.huang@anu.edu.au>
2018-09-04 16:53:41 +1000

    test

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangotest'
+    'djangotest',
 ]
 
 MIDDLEWARE = [
@@ -129,7 +129,3 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
