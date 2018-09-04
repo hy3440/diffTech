613e069801fb40241d2d3791b53d3b846231aaf5
Yi  Huang <yi.huang@anu.edu.au>
2018-09-04 16:50:21 +1000

    test

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -129,3 +129,6 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
